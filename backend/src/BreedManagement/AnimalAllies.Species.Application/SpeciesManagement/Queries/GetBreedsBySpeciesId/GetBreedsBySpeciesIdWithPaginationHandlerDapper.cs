094d29ce61e21385c00bb1410bc32b3f7b715fee
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-04-22 16:17:49 +0300

    feat: added caching for breeds management queries

--- backend/src/BreedManagement/AnimalAllies.Species.Application/SpeciesManagement/Queries/GetBreedsBySpeciesId/GetBreedsBySpeciesIdWithPaginationHandlerDapper.cs
+++ backend/src/BreedManagement/AnimalAllies.Species.Application/SpeciesManagement/Queries/GetBreedsBySpeciesId/GetBreedsBySpeciesIdWithPaginationHandlerDapper.cs
@@ -8,6 +8,7 @@ using AnimalAllies.SharedKernel.Constraints;
 using AnimalAllies.SharedKernel.Shared;
 using Dapper;
 using FluentValidation;
+using Microsoft.Extensions.Caching.Hybrid;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -15,18 +16,23 @@ namespace AnimalAllies.Species.Application.SpeciesManagement.Queries.GetBreedsBy
 
 public class GetBreedsBySpeciesIdWithPaginationHandlerDapper: IQueryHandler<PagedList<BreedDto>, GetBreedsBySpeciesIdWithPaginationQuery>
 {
+    private const string REDIS_KEY = "breeds_";
+    
     private readonly ISqlConnectionFactory _sqlConnectionFactory;
     private readonly IValidator<GetBreedsBySpeciesIdWithPaginationQuery> _validator;
     private readonly ILogger<GetBreedsBySpeciesIdWithPaginationHandlerDapper> _logger;
+    private readonly HybridCache _hybridCache;
 
     public GetBreedsBySpeciesIdWithPaginationHandlerDapper(
         [FromKeyedServices(Constraints.Context.BreedManagement)]ISqlConnectionFactory sqlConnectionFactory,
         ILogger<GetBreedsBySpeciesIdWithPaginationHandlerDapper> logger,
-        IValidator<GetBreedsBySpeciesIdWithPaginationQuery> validator)
+        IValidator<GetBreedsBySpeciesIdWithPaginationQuery> validator,
+        HybridCache hybridCache)
     {
         _sqlConnectionFactory = sqlConnectionFactory;
         _logger = logger;
         _validator = validator;
+        _hybridCache = hybridCache;
     }
 
     public async Task<Result<PagedList<BreedDto>>> Handle(GetBreedsBySpeciesIdWithPaginationQuery query, CancellationToken cancellationToken = default)
@@ -49,23 +55,32 @@ public class GetBreedsBySpeciesIdWithPaginationHandlerDapper: IQueryHandler<Page
                                         from species.breeds
                                     where species_id = @SpeciesId
                                     """);
-        
+
         sql.ApplySorting(query.SortBy, query.SortDirection);
         sql.ApplyPagination(query.Page,query.PageSize);
-
-        var breeds = await connection.QueryAsync<BreedDto>(sql.ToString(), parameters);
+        
+        var options = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromHours(24)
+        };
+        
+        var cachedBreeds = await _hybridCache.GetOrCreateAsync(
+            key: REDIS_KEY + query.GetHashCode(),
+            factory: async _ => await connection.QueryAsync<BreedDto>(sql.ToString(), parameters),
+            options: options,
+            cancellationToken: cancellationToken);
         
         _logger.LogInformation("Get breeds with pagination Page: {Page}, PageSize: {PageSize}",
             query.Page, query.PageSize);
 
-        var breedsDtos = breeds.ToList();
+        var breedsDtos = cachedBreeds.ToList();
         
         return new PagedList<BreedDto>
         {
             Items = breedsDtos.ToList(),
             PageSize = query.PageSize,
             Page = query.Page,
-            TotalCount = breedsDtos.Count()
+            TotalCount = breedsDtos.Count
         };
 
     }
@@ -87,12 +102,20 @@ public class GetBreedsBySpeciesIdWithPaginationHandlerDapper: IQueryHandler<Page
                                     where species_id = @SpeciesId
                                     """);
         
-
-        var breeds = await connection.QueryAsync<BreedDto>(sql.ToString(), parameters);
+        var options = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromHours(24)
+        };
+        
+        var cachedBreeds = await _hybridCache.GetOrCreateAsync(
+            key: REDIS_KEY,
+            factory: async _ => await connection.QueryAsync<BreedDto>(sql.ToString(), parameters),
+            options: options,
+            cancellationToken: cancellationToken);
         
         _logger.LogInformation("Get breeds with pagination with {speciesId}", speciesId);
 
-        return breeds.ToList();
+        return cachedBreeds.ToList();
     }
     
 }
--- backend/src/BreedManagement/AnimalAllies.Species.Application/SpeciesManagement/Queries/GetSpeciesWithPagination/GetSpeciesWithPaginationHandlerDapper.cs
+++ backend/src/BreedManagement/AnimalAllies.Species.Application/SpeciesManagement/Queries/GetSpeciesWithPagination/GetSpeciesWithPaginationHandlerDapper.cs
@@ -8,6 +8,7 @@ using AnimalAllies.SharedKernel.Constraints;
 using AnimalAllies.SharedKernel.Shared;
 using Dapper;
 using FluentValidation;
+using Microsoft.Extensions.Caching.Hybrid;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -15,17 +16,23 @@ namespace AnimalAllies.Species.Application.SpeciesManagement.Queries.GetSpeciesW
 
 public class GetSpeciesWithPaginationHandlerDapper : IQueryHandler<PagedList<SpeciesDto>, GetSpeciesWithPaginationQuery>
 {
+    private const string REDIS_KEY = "species_";
+    
     private readonly ISqlConnectionFactory _sqlConnectionFactory;
     private readonly IValidator<GetSpeciesWithPaginationQuery> _validator;
     private readonly ILogger<GetSpeciesWithPaginationHandlerDapper> _logger;
+    private readonly HybridCache _hybridCache;
 
     public GetSpeciesWithPaginationHandlerDapper(
         [FromKeyedServices(Constraints.Context.BreedManagement)]ISqlConnectionFactory sqlConnectionFactory,
-        ILogger<GetSpeciesWithPaginationHandlerDapper> logger, IValidator<GetSpeciesWithPaginationQuery> validator)
+        ILogger<GetSpeciesWithPaginationHandlerDapper> logger,
+        IValidator<GetSpeciesWithPaginationQuery> validator,
+        HybridCache hybridCache)
     {
         _sqlConnectionFactory = sqlConnectionFactory;
         _logger = logger;
         _validator = validator;
+        _hybridCache = hybridCache;
     }
     
     public async Task<Result<PagedList<SpeciesDto>>> Handle(GetSpeciesWithPaginationQuery query, CancellationToken cancellationToken = default)
@@ -47,20 +54,29 @@ public class GetSpeciesWithPaginationHandlerDapper : IQueryHandler<PagedList<Spe
         
         sql.ApplySorting(query.SortBy, query.SortDirection);
         sql.ApplyPagination(query.Page,query.PageSize);
-
-        var species = await connection.QueryAsync<SpeciesDto>(sql.ToString(), parameters);
+        
+        var options = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromHours(24)
+        };
+        
+        var cachedSpecies = await _hybridCache.GetOrCreateAsync(
+            key: REDIS_KEY + query.GetHashCode(),
+            factory: async _ => await connection.QueryAsync<SpeciesDto>(sql.ToString(), parameters),
+            options: options,
+            cancellationToken: cancellationToken);
         
         _logger.LogInformation("Get species with pagination Page: {Page}, PageSize: {PageSize}",
             query.Page, query.PageSize);
 
-        var speciesDtos = species.ToList();
+        var speciesDtos = cachedSpecies.ToList();
         
         return new PagedList<SpeciesDto>
         {
             Items = speciesDtos.ToList(),
             PageSize = query.PageSize,
             Page = query.Page,
-            TotalCount = speciesDtos.Count()
+            TotalCount = speciesDtos.Count
         };
 
     }
