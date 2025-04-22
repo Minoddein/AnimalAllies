eea521d021516447d7810b43c5b06003dba87ff2
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-04-23 01:21:35 +0300

    feat: added caching for GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler.cs

--- backend/src/VolunteerRequests/VolunteerRequests.Application/Features/Queries/GetFilteredVolunteerRequestsByAdminIdWithPagination/GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler.cs
+++ backend/src/VolunteerRequests/VolunteerRequests.Application/Features/Queries/GetFilteredVolunteerRequestsByAdminIdWithPagination/GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler.cs
@@ -10,6 +10,7 @@ using AnimalAllies.SharedKernel.Constraints;
 using AnimalAllies.SharedKernel.Shared;
 using Dapper;
 using FluentValidation;
+using Microsoft.Extensions.Caching.Hybrid;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using VolunteerRequests.Domain.ValueObjects;
@@ -19,18 +20,23 @@ namespace VolunteerRequests.Application.Features.Queries.GetFilteredVolunteerReq
 public class GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler:
     IQueryHandler<PagedList<VolunteerRequestDto>, GetFilteredVolunteerRequestsByAdminIdWithPaginationQuery>
 {
+    private const string REDIS_KEY = "volunteer-requests_";
+    
     private readonly ISqlConnectionFactory _sqlConnectionFactory;
     private readonly IValidator<GetFilteredVolunteerRequestsByAdminIdWithPaginationQuery> _validator;
     private readonly ILogger<GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler> _logger;
-
+    private readonly HybridCache _hybridCache;
+    
     public GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler(
         [FromKeyedServices(Constraints.Context.VolunteerRequests)]ISqlConnectionFactory sqlConnectionFactory,
         IValidator<GetFilteredVolunteerRequestsByAdminIdWithPaginationQuery> validator, 
-        ILogger<GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler> logger)
+        ILogger<GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler> logger,
+        HybridCache hybridCache)
     {
         _sqlConnectionFactory = sqlConnectionFactory;
         _validator = validator;
         _logger = logger;
+        _hybridCache = hybridCache;
     }
 
     public async Task<Result<PagedList<VolunteerRequestDto>>> Handle(
@@ -41,62 +47,71 @@ public class GetFilteredVolunteerRequestsByAdminIdWithPaginationHandler:
         if (!validationResult.IsValid)
             return validationResult.ToErrorList();
         
-        var connection = _sqlConnectionFactory.Create();
+        var cacheKey = $"{REDIS_KEY}{query.AdminId}" +
+                       $":status_{query.RequestStatus}" +
+                       $":sort_{query.SortBy}" +
+                       $":dir_{query.SortDirection}" +
+                       $":page_{query.Page}" +
+                       $":size_{query.PageSize}";
 
-        var parameters = new DynamicParameters();
-        parameters.Add("@AdminId", query.AdminId);
+        var options = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromMinutes(2)
+        };
         
-        var sql = new StringBuilder("""
-                                    select 
-                                        id,
-                                        first_name,
-                                        second_name,
-                                        patronymic,
-                                        description,
-                                        email,
-                                        phone_number,
-                                        work_experience,
-                                        admin_id,
-                                        user_id,
-                                        discussion_id,
-                                        request_status,
-                                        rejection_comment,
-                                        social_networks
-                                        from volunteer_requests.volunteer_requests 
-                                        where admin_id = @AdminId
-                                    """);
-        bool hasWhereClause = true;
+        var result = await _hybridCache.GetOrCreateAsync(
+            key:cacheKey,
+            factory:async cancelToken =>
+            {
+                var connection = _sqlConnectionFactory.Create();
+                var parameters = new DynamicParameters();
+                parameters.Add("@AdminId", query.AdminId);
+                
+                var sql = new StringBuilder("""
+                    SELECT 
+                        id, first_name, second_name, patronymic,
+                        description, email, phone_number, work_experience,
+                        admin_id, user_id, discussion_id, request_status,
+                        rejection_comment, social_networks
+                    FROM volunteer_requests.volunteer_requests 
+                    WHERE admin_id = @AdminId
+                    """);
 
-        var stringProperties = new Dictionary<string, string>(){ { "request_status", query.RequestStatus } };
+                var hasWhereClause = true;
+                var stringProperties = new Dictionary<string, string>() { { "request_status", query.RequestStatus } };
+                sql.ApplyFilterByString(ref hasWhereClause, stringProperties);
+                
+                sql.ApplySorting(query.SortBy, query.SortDirection);
+                sql.ApplyPagination(query.Page, query.PageSize);
+
+                var requests = await connection.QueryAsync<VolunteerRequestDto, SocialNetworkDto[], VolunteerRequestDto>(
+                    sql.ToString(),
+                    (volunteerRequest, socialNetworks) =>
+                    {
+                        volunteerRequest.SocialNetworks = socialNetworks;
+                        return volunteerRequest;
+                    },
+                    splitOn: "social_networks",
+                    param: parameters);
+
+                var list = requests.ToList();
+
+                return list;
+            },
+            options:options,
+            cancellationToken: cancellationToken);
         
-        sql.ApplyFilterByString(ref hasWhereClause, stringProperties);
         
-        sql.ApplySorting(query.SortBy,query.SortDirection);
-        
-        sql.ApplyPagination(query.Page,query.PageSize);
-        
-        var volunteerRequests = 
-            await connection.QueryAsync<VolunteerRequestDto, SocialNetworkDto[], VolunteerRequestDto>(
-                sql.ToString(),
-                (volunteerRequest, socialNetworks) =>
-                {
-                    volunteerRequest.SocialNetworks = socialNetworks;
-                    return volunteerRequest;
-                },
-                splitOn:"social_networks",
-                param: parameters);
         
         _logger.LogInformation("Get volunteer requests with pagination Page: {Page}, PageSize: {PageSize}",
             query.Page, query.PageSize);
-
-        var volunteerRequestDtos = volunteerRequests.ToList();
         
         return new PagedList<VolunteerRequestDto>
         {
-            Items = volunteerRequestDtos,
+            Items = result,
             PageSize = query.PageSize,
             Page = query.Page,
-            TotalCount = volunteerRequestDtos.Count()
+            TotalCount = result.Count
         };
     }
 }
