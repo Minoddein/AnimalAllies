c2cbcf1c4375c12692f8e1349eaf9657002c29af
Minoddein <kamazitun.jaykehd@gmail.com>
2025-05-03 03:58:18 +0300

    feat: added tag constants

---
+++ backend/src/Shared/AnimalAllies.SharedKernel/CachingConstants/TagsConstants.cs
@@ -0,0 +1,13 @@
+namespace AnimalAllies.SharedKernel.CachingConstants;
+
+public static class TagsConstants
+{
+    public const string ACCOUNTS = "accounts";
+    public const string BREEDS = "breeds";
+    public const string SPECIES = "species";
+    public const string PETS = "pets";
+    public const string VOLUNTEERS = "volunteers";
+    public const string DISCUSSIONS = "discussions";
+    public const string MESSAGES = "messages";
+    public const string VOLUNTEER_REQUESTS = "volunteers_requests";
+}
